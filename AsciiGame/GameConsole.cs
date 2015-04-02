fda3941275cbd361d230c9da40aa0b1523a0e941
Ytrog <hisbrucker@gmail.com>
2015-04-02 22:03:12 +0200

    basic layout of project

---
+++ AsciiGame/GameConsole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsciiGame
+{
+    class GameConsole : Component
+    {
+        private bool _running;
+        public void Start()
+        {
+            _running = true;
+            while (_running)
+            {
+                HandleInput();   
+                
+            }
+        }
+
+        protected virtual void HandleInput()
+        {
+            
+        }
+    }
+}
